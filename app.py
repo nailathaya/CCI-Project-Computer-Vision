3710ffb03d161f95dc2bdb591d0b16515c6818a1
nailathaya <athhayamumtaz@gmail.com>
2025-09-28 23:12:46 +0700

    realtime detection with streamlit

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+import av
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("models/best.pt")
+
+st.title("Deteksi Objek Realtime dengan Kamera")
+
+class VideoProcessor(VideoProcessorBase):
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        
+        results = model(img, conf=0.5)
+
+        annotated_frame = results[0].plot()
+        
+        return av.VideoFrame.from_ndarray(annotated_frame, format="bgr24")
+
+webrtc_streamer(
+    key="example",
+    video_processor_factory=VideoProcessor,
+    media_stream_constraints={"video": True, "audio": False},
+)
